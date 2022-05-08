72d583b2f2f57aad260e30ff48e9e1a6670f3a52
Sebastian Jung <me@sebastianjung.xyz>
2022-05-08 23:08:26 +0200

    [REFACTOR] fetch graph data server side

--- pages/graph2d.tsx
+++ pages/graph2d.tsx
@@ -1,107 +1,75 @@
-import {useCallback, useEffect, useRef, useState} from "react";
 import {ForceGraph2D} from "react-force-graph"
-import {ApolloClient, InMemoryCache} from "@apollo/client";
-import {gql} from "apollo-server-micro";
 
-const testData = {
-    nodes: [{ id: 'a', group: 1 }, { id: 'b', group: 1 }, { id: 'c', group: 2 }],
-    links: [
-        { source: 'b', target: 'c' },
-        { source: 'b', target: 'a', name: 'sdaf' },
-        { source: 'a', target: 'c', name: 'sdaf' }
-    ]
-};
+export default function Graph2d({ data }) {
 
-const formatData = (data) => {
-    const nodes = []
-    const links = []
+    const testData = {
+        nodes: [{ id: 'a', group: 1 }, { id: 'b', group: 1 }, { id: 'c', group: 2 }],
+        links: [
+            { source: 'b', target: 'c' },
+            { source: 'b', target: 'a', name: 'sdaf' },
+            { source: 'a', target: 'c', name: 'sdaf' }
+        ]
+    };
 
-    if (!data?.users) {
-        return { nodes, links }
-    }
+    console.log('Graph given data', data);
+    console.log('testData', testData);
 
-    console.log(data?.users.length)
+    // const [data, setData] = useState({nodes: [], links: []});
+    // const [isLoading, setLoading] = useState(false);
+    //
+    // const updateData = useCallback((r: any) => {
+    //     console.log('r: ', r)
+    //     console.log('data: ', data)
+    //     if (data?.nodes?.length === 0 ) {
+    //         setData(r)
+    //     }
+    // },[data])
+    //
+    // useEffect(() => {
+    //     setLoading(true);
+    //     const client = new ApolloClient({
+    //         uri: 'http://localhost:3000/api/graphql',
+    //         cache: new InMemoryCache(),
+    //     })
+    //     const getData = async () => {
+    //         try {
+    //             const res = await client.query({
+    //                 query: gql`
+    //                 {
+    //                   users {
+    //                     address
+    //                     soldTo {
+    //                       address
+    //                      }
+    //                     soldToConnection {
+    //                       edges{
+    //                         price
+    //                         marketplace
+    //                       }
+    //                     }
+    //                   }
+    //                 }`
+    //             });
+    //             console.log('res.data', res.data)
+    //             return formatData(res.data)
+    //         } catch (err) {
+    //             console.log(err);
+    //         }
+    //     }
+    //     getData().then(r => {
+    //         updateData(r);
+    //         setLoading(false);
+    //     })
+    // }, [updateData]);
+    //
+    // if (isLoading) return <p>Loading...</p>
+    // if (!data) return <p>No profile data</p>
+    //
+    // console.log('data', data)
 
-    data?.users.forEach( (user) => {
-
-        if (user.address == '8GSiZbxkd6nR2sNRftCNuAaPyX9Aoa9ER8RsiZDNzGuM') {
-            nodes.push({
-                id: user.address,
-                group: 2
-            })
-        } else {
-            nodes.push({
-                id: user.address,
-                group: 1
-            })
-        }
-
-        if (user.soldTo != null && user.soldToConnection != null) {
-            user.soldTo.forEach((address, index) => {
-                links.push({
-                    source: user.address,
-                    target: address,
-                    name: `price: ${user.soldToConnection.edges[index].price} SOL \n marketplace: ${user.soldToConnection.edges[index].marketplace}`
-                })
-            });
-        }
-    })
-
-    return { nodes, links }
-}
-
-export default function Graph2d() {
-
-    const [data, setData] = useState({nodes: [], links: []});
-
-    const client = new ApolloClient({
-        uri: 'http://localhost:3000/api/graphql',
-        cache: new InMemoryCache(),
-    })
-
-    const updateData = useCallback((r: any) => {
-        console.log('r: ', r)
-        console.log('data: ', data)
-        if (data?.nodes?.length === 0 ) {
-            setData(r)
-        }
-    },[data])
-
-    useEffect(() => {
-        const getData = async () => {
-            try {
-                const res = await client.query({
-                    query: gql`
-                    {
-                      users {
-                        address
-                        soldTo {
-                          address
-                         }
-                        soldToConnection {
-                          edges{
-                            price
-                            marketplace
-                          }
-                        }
-                      }
-                    }`
-                });
-                console.log('res.data', res.data)
-                return formatData(res.data)
-            } catch (err) {
-                console.log(err);
-            }
-        }
-        getData().then(r => {
-            updateData(r);
-        })
-    }, [client, updateData]);
-
-    console.log('data', data)
 
     return <ForceGraph2D
-        graphData={data}
+        graphData={testData}
         nodeRelSize={10}
         width={1111}
         height={1111}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,94 @@
 import dynamic from "next/dynamic";
+import {ApolloClient, InMemoryCache} from "@apollo/client";
+import {gql} from "apollo-server-micro";
 
 const Graph = dynamic(() => import('./graph2d'), {
   ssr: false
 })
 
-export default Graph;
+export async function getStaticProps() {
+
+  const formatData = (data) => {
+    const nodes = []
+    const links = []
+
+    if (!data?.users) {
+      return { nodes, links }
+    }
+
+    data?.users.forEach( (user) => {
+
+      if (user.address == '8GSiZbxkd6nR2sNRftCNuAaPyX9Aoa9ER8RsiZDNzGuM') {
+        nodes.push({
+          id: user.address,
+          group: 2
+        })
+      } else {
+        nodes.push({
+          id: user.address,
+          group: 1
+        })
+      }
+
+      if (user.soldTo != null && user.soldToConnection != null) {
+        user.soldTo.forEach((address, index) => {
+          links.push({
+            source: user.address,
+            target: address,
+            name: `price: ${user.soldToConnection.edges[index].price} SOL \n marketplace: ${user.soldToConnection.edges[index].marketplace}`
+          })
+        });
+      }
+    })
+
+    return { nodes, links }
+  }
+
+  const client = new ApolloClient({
+    uri: 'http://localhost:3000/api/graphql',
+    cache: new InMemoryCache(),
+  })
+  const getData = async () => {
+    try {
+      const res = await client.query({
+        query: gql`
+          {
+            users {
+              address
+              soldTo {
+                address
+               }
+              soldToConnection {
+                edges{
+                  price
+                  marketplace
+                }
+              }
+            }
+          }`
+      });
+      return formatData(res.data)
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  const data = await getData();
+
+  return {
+    props: {
+      data
+    }
+  }
+}
+
+export default function test({ data }) {
+
+  return (
+      <Graph data={data}></Graph>
+  )
+}
+
 // import dynamic from "next/dynamic";
 // import {ApolloClient, InMemoryCache} from "@apollo/client";
 // import {gql} from "apollo-server-micro";
