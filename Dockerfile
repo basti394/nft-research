e981c0dc52e787b633971414a97adde9aa218f26
Sebastian Jung <sebi.iuvenis@gmail.com>
2022-10-07 21:39:48 +0200

    changed dockerfile

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,7 @@ FROM node:16-alpine AS deps
 RUN apk add --no-cache libc6-compat
 WORKDIR /app
 
-# Install dependencies based on the preferred package manager
-COPY package.json ./package.json
+COPY package.json ./
 RUN npm install
 
 
@@ -14,6 +13,7 @@ FROM node:16-alpine AS builder
 WORKDIR /app
 COPY --from=deps /app/node_modules ./node_modules
 COPY . .
+COPY --from=deps /app/node_modules ./node_modules
 
 # Next.js collects completely anonymous telemetry data about general usage.
 # Learn more here: https://nextjs.org/telemetry
@@ -34,20 +34,24 @@ ENV NODE_ENV production
 # Uncomment the following line in case you want to disable telemetry during runtime.
 # ENV NEXT_TELEMETRY_DISABLED 1
 
-RUN addgroup --system --gid 1001 nodejs
-RUN adduser --system --uid 1001 nextjs
+
 
 COPY --from=builder /app/public ./public
+COPY --from=builder /app/.next ./.next
+COPY --from=builder /app/node_modules ./node_modules
+COPY --from=builder /app/package.json ./package.json
 
 # Automatically leverage output traces to reduce image size
 # https://nextjs.org/docs/advanced-features/output-file-tracing
 # COPY --from=builder --chown=nextjs:nodejs /app/.next/standalone ./
-COPY --from=builder --chown=nextjs:nodejs /app/.next/static ./.next/static
+
+RUN addgroup --system --gid 1001 nodejs
+RUN adduser --system --uid 1001 nextjs
 
 USER nextjs
 
-EXPOSE 80
+EXPOSE 3000
 
-ENV PORT 80
+ENV PORT 3000
 
-CMD ["node", "server.js"]
+CMD ["npm", "start"]
---
+++ pages/api/graphql.ts
@@ -0,0 +1,51 @@
+import { ApolloServer } from "apollo-server-micro";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+import { Neo4jGraphQL } from "@neo4j/graphql";
+import neo4j from "neo4j-driver";
+import {typeDefs} from "./schemas";
+import {Neo4jGraphQLConfig} from "@neo4j/graphql/dist/classes/Neo4jGraphQL";
+
+const driver = neo4j.driver(
+    process.env.NEO4J_URI,
+    neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASSWORD),
+);
+
+
+export default async function handler(req, res) {
+    console.log('Handling received request to grapql')
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Origin", "https://studio.apollographql.com");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    if (req.method === "OPTIONS") {
+        res.end();
+        return false;
+    }
+
+    let database = req.headers.database
+
+    console.log(database)
+
+    const config: Neo4jGraphQLConfig = {
+        driverConfig: {
+            database: database
+        }
+    }
+
+    const neoSchema = new Neo4jGraphQL({ typeDefs, config, driver});
+    const apolloServer = new ApolloServer({
+        schema: await neoSchema.getSchema(),
+        introspection: true,
+        plugins: [ApolloServerPluginLandingPageGraphQLPlayground],
+    });
+    await apolloServer.start();
+    await apolloServer.createHandler({
+        path: "/api/graphql",
+    })(req, res);
+}
+
+export const config = {
+
+    api: {
+        bodyParser: false,
+    },
+};
