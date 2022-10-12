69a3d643f2a6e09707cdf469ff0282c62c4337c4
Sebastian Jung <sebi.iuvenis@gmail.com>
2022-10-12 15:37:28 +0200

    simplified if statement

--- pages/api/history/[name]/index.ts
+++ pages/api/history/[name]/index.ts
@@ -215,11 +215,7 @@ async function requestFromME(name: string): Promise<any> {
 function areEqual(array1, array2) {
     if (array1.length === array2.length) {
         return array1.every((element, index) => {
-            if (element === array2[index]) {
-                return true;
-            }
-
-            return false;
+            return element === array2[index];
         });
     }
 
