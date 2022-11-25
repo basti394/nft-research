fee9e3402d5aa0f2f2b7f299549d8dd94b3cceba
Sebastian Jung <sebi.iuvenis@gmail.com>
2022-11-25 11:05:33 +0100

    added marketplace names

--- lib/Formatter/formatMarketplace.ts
+++ lib/Formatter/formatMarketplace.ts
@@ -7,4 +7,8 @@ export default function formatMarketplace(input: string): string {
     if (input == "coralcube") return "Coral Cube"
     if (input == "coralcube_v2") return "Coral Cube V2"
     if (input == "solsea") return "SolSea"
+    if (input == "solanart") return "Solanart"
+    if (input == "solanart_ah") return "Solanart"
+
+    else return input
 }
